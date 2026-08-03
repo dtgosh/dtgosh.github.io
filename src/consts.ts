1de2b57692c8bb7bbbbdec545ac3250ca6183472
dtgosh <90609214+dtgosh@users.noreply.github.com>
2026-08-04 00:18:27 +0900

    refactor: 메인 페이지 제목과 소개 문구를 사이트 상수로 통일

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'dtgosh';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '잊고 싶지 않은 것과 공유드리고 싶은 것을 작성하겠습니다.';
